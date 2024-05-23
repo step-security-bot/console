f08c0ff8d6899dd3081d9dcb5b37ab6cc68a2437
Ethan Meeker <ethan.meeker@intel.com>
2024-05-23 15:17:11 -0700

    wip

--- internal/usecase/devices/interceptor_test.go
+++ internal/usecase/devices/interceptor_test.go
@@ -1,14 +1,10 @@
 package devices_test
 
 import (
-	"context"
 	"testing"
 
-	"github.com/gorilla/websocket"
-	"github.com/open-amt-cloud-toolkit/console/internal/entity/dto"
 	devices "github.com/open-amt-cloud-toolkit/console/internal/usecase/devices"
 	"github.com/open-amt-cloud-toolkit/console/pkg/logger"
-	"github.com/open-amt-cloud-toolkit/go-wsman-messages/v2/pkg/wsman/client"
 	gomock "go.uber.org/mock/gomock"
 )
 
@@ -26,34 +22,34 @@ func initInterceptorTest(t *testing.T) (*devices.UseCase, *MockRedirection, *Moc
 	return u, redirect, repo
 }
 
-type redirectTest struct {
-	name     string
-	redMock  func(*MockRedirection)
-	repoMock func(*MockRepository)
-	//res      any
-	err error
-}
+// type redirectTest struct {
+// 	name     string
+// 	redMock  func(*MockRedirection)
+// 	repoMock func(*MockRepository)
+// 	//res      any
+// 	err error
+// }
 
-func TestListenToDevice(t *testing.T) {
-	ctx := context.Background()
-	conn := &websocket.Conn{}
-	device := &dto.Device{
-		GUID:     "test-guid",
-		Username: "user",
-		Password: "password",
-	}
-	deviceConnection := &devices.DeviceConnection{
-		Conn:      conn,
-		Device:    *device,
-		Direct:    false,
-		Challenge: client.AuthChallenge{},
-	}
+// func TestListenToDevice(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := &websocket.Conn{}
+// 	device := &dto.Device{
+// 		GUID:     "test-guid",
+// 		Username: "user",
+// 		Password: "password",
+// 	}
+// 	deviceConnection := &devices.DeviceConnection{
+// 		Conn:      conn,
+// 		Device:    *device,
+// 		Direct:    false,
+// 		Challenge: client.AuthChallenge{},
+// 	}
 
-	useCase, redirect, _ := initInterceptorTest(t)
+// 	useCase, redirect, _ := initInterceptorTest(t)
 
-	redirect.EXPECT().RedirectListen(gomock.Any(), deviceConnection).Return([]byte{}, nil)
-	redirect.EXPECT().RedirectListen(gomock.Any(), deviceConnection).Return([]byte{1, 2, 3}, nil)
+// 	redirect.EXPECT().RedirectListen(gomock.Any(), deviceConnection).Return([]byte{}, nil)
+// 	redirect.EXPECT().RedirectListen(gomock.Any(), deviceConnection).Return([]byte{1, 2, 3}, nil)
 
-	useCase.ListenToDevice(ctx, deviceConnection)
+// 	useCase.ListenToDevice(ctx, deviceConnection)
 
-}
+// }
--- internal/usecase/devices/redirection_test.go
+++ internal/usecase/devices/redirection_test.go
@@ -3,12 +3,14 @@ package devices_test
 import (
 	"testing"
 
+	"github.com/open-amt-cloud-toolkit/console/internal/entity/dto"
 	devices "github.com/open-amt-cloud-toolkit/console/internal/usecase/devices"
-	"github.com/open-amt-cloud-toolkit/console/pkg/logger"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/v2/pkg/wsman"
+	"github.com/stretchr/testify/require"
 	gomock "go.uber.org/mock/gomock"
 )
 
-func initRedirectionTest(t *testing.T) (*devices.UseCase, *MockRedirection, *MockRepository) {
+func initRedirectionTest(t *testing.T) (*devices.Redirector, *MockRedirection, *MockRepository) {
 	t.Helper()
 
 	mockCtl := gomock.NewController(t)
@@ -16,16 +18,127 @@ func initRedirectionTest(t *testing.T) (*devices.UseCase, *MockRedirection, *Moc
 
 	repo := NewMockRepository(mockCtl)
 	redirect := NewMockRedirection(mockCtl)
-	log := logger.New("error")
-	u := devices.New(repo, nil, redirect, log)
+	u := &devices.Redirector{}
 
 	return u, redirect, repo
 }
 
 type redTest struct {
-	name     string
-	redMock  func(*MockRedirection)
-	repoMock func(*MockRepository)
-	res      any
-	err      error
+	name    string
+	redMock func(*MockRedirection)
+	//repoMock func(*MockRepository)
+	res any
 }
+
+func TestSetupWsmanClient(t *testing.T) {
+	t.Parallel()
+
+	device := &dto.Device{
+		GUID:     "device-guid-123",
+		TenantID: "tenant-id-456",
+	}
+
+	tests := []redTest{
+		{
+			name: "success",
+			redMock: func(redirect *MockRedirection) {
+				redirect.EXPECT().
+					SetupWsmanClient(gomock.Any(), false, true).
+					Return(wsman.Messages{})
+			},
+			res: wsman.Messages{},
+		},
+		{
+			name: "fail",
+			redMock: func(redirect *MockRedirection) {
+				redirect.EXPECT().
+					SetupWsmanClient(gomock.Any(), true, true).
+					Return(wsman.Messages{})
+			},
+			res: wsman.Messages{},
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc // Necessary for proper parallel execution
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			redirector, redirect, _ := initRedirectionTest(t)
+
+			tc.redMock(redirect)
+
+			res := redirector.SetupWsmanClient(*device, true, true)
+
+			require.IsType(t, tc.res, res)
+		})
+	}
+}
+
+func TestNewRedirector(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "success",
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc // Necessary for proper parallel execution
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Call the function under test
+			redirector := devices.NewRedirector()
+
+			// Assert that the returned redirector is not nil
+			require.NotNil(t, redirector)
+		})
+	}
+}
+
+// func TestRedirectConnect(t *testing.T) {
+// 	t.Parallel()
+
+// 	device := &dto.Device{
+// 		GUID:     "device-guid-123",
+// 		TenantID: "tenant-id-456",
+// 	}
+// 	conn := &websocket.Conn{}
+// 	deviceConnection := &devices.DeviceConnection{
+// 		Conn:      conn,
+// 		Device:    *device,
+// 		Direct:    false,
+// 		Challenge: client.AuthChallenge{},
+// 	}
+
+// 	tests := []redTest{
+// 		{
+// 			name: "success",
+// 			redMock: func(redirect *MockRedirection) {
+// 				redirect.EXPECT().
+// 					SetupWsmanClient(gomock.Any(), false, true).
+// 					Return(wsman.Messages{})
+// 			},
+// 			res: nil,
+// 		},
+// 	}
+
+// 	for _, tc := range tests {
+// 		tc := tc // Necessary for proper parallel execution
+// 		t.Run(tc.name, func(t *testing.T) {
+// 			t.Parallel()
+
+// 			redirector, redirect, _ := initRedirectionTest(t)
+
+// 			tc.redMock(redirect)
+
+// 			err := redirector.RedirectConnect(context.Background(), deviceConnection)
+
+// 			require.IsType(t, tc.err, err)
+// 		})
+// 	}
+// }
